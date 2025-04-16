3ffc67e5b6e92912179c423deb26f9918c85105b
Kazoom <welovekazim@gmail.com>
2025-04-16 18:00:08 +0530

    Update expt 2.py

--- expt 2.py
+++ expt 2.py
@@ -1,1 +1,18 @@
+'''
+Mohd Qazim Ansari
+Fe-D
+241P105/36
+'''
+
+import math
+
+print("Choose a geometric figure to calculate its area:")
+print("1. Circle")
+print("2. Rectangle")
+print("3. Triangle")
+
+choice = input("Enter your choice (1/2/3): ")
+
+if choice == '1':
+    radius = float(input("Enter the radius of the circle: "))
 
