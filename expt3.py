8fd919d3f3003b3f881270bc44a6aff69787dad0
Kazoom <welovekazim@gmail.com>
2025-04-16 18:03:26 +0530

    Update expt3.py

--- expt3.py
+++ expt3.py
@@ -1,1 +1,22 @@
 
+'''
+Mohd Qazim Ansari
+Fe-D
+241P105/36
+'''
+
+import math
+
+bs = float(input("Enter the basic salary (BS): "))
+
+da = math.ceil(bs * 0.70)
+ta = math.ceil(bs * 0.30)
+hra = math.ceil(bs * 0.10)
+
+gross_salary = bs + da + ta + hra
+
+print(f"Basic Salary (BS):        {bs:.2f}\n")
+print(f"Dearness Allowance:       {da:.2f}")
+print(f"Travel Allowance:         {ta:.2f}")
+print(f"House Rent Allowance:     {hra:.2f}")
+print(f"Gross Salary:             {gross_salary:.2f}")
