0b268ac2cbedbac7c4bd80e2e56a857a8c1dab53
Kazoom <welovekazim@gmail.com>
2025-04-16 18:16:09 +0530

    Update expt 9.py

--- expt 9.py
+++ expt 9.py
@@ -1,1 +1,17 @@
+'''
+MOHD QAZIM ANSARI
+241P105/36
+FE-D
+27/02/25
+'''
+
+n = int(input("enter the number :"))
+
+if n < 0:
+    print("factorial is not defined for the negative number")
+else:
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    print(f"factorial of {n} is {result}")
 
