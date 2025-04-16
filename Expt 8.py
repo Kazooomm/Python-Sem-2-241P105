107716046526b218be3ecd2a0c591549741d9ce7
Kazoom <welovekazim@gmail.com>
2025-04-16 18:12:14 +0530

    Update Expt 8.py

--- Expt 8.py
+++ Expt 8.py
@@ -1,1 +1,31 @@
+'''
+Mohd Qazim Ansari
+Fe-D
+241P105/36
+'''
+
+# Function to check even or odd
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Main program
+def main():
+    try:
+        # Input: Number of values to check
+        count = int(input("Enter how many numbers you want to check: "))
+
+        # Loop through each number
+        for i in range(count):
+            num = int(input(f"Enter number {i + 1}: "))
+            result = check_even_odd(num)
+            print(f"{num} is {result}.")
+
+    except ValueError:
+        print("Invalid input. Please enter valid integers.")
+
+# Run the program
+main()
 
