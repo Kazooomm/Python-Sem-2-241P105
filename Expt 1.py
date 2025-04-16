9f3b8b4c2758b62e73d4ccfcf7e1070d044d560b
Kazoom <welovekazim@gmail.com>
2025-04-16 17:55:39 +0530

    Update Expt 1.py

--- Expt 1.py
+++ Expt 1.py
@@ -1,1 +1,11 @@
+'''
+Mohd Qazim Ansari
+Fe-D
+241P105/36
+'''
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print(f"Hello, {name}! It's great to know you're {age} years old. Have a wonderful day!")
 
